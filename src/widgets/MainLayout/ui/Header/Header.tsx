a271231981d2b1c888bb2eb35977fe01181e20ee
pvslvch <polina.vasilevichh@gmail.com>
2026-04-15 12:39:06 +0200

    feat(navigation): implement app navigation

--- src/widgets/MainLayout/ui/Header/Header.tsx
+++ src/widgets/MainLayout/ui/Header/Header.tsx
@@ -1,5 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
 import cls from "./Header.module.css";
 
+import LogoImage from "./logo.png";
+
 export function Header() {
-  return <header className={cls.header}>Header</header>;
+  return (
+    <header className={cls.header}>
+      <Link href="/">
+        <Image width={40} src={LogoImage} alt="Youtube Logo Image" />
+      </Link>
+    </header>
+  );
 }
--- src/widgets/MainLayout/ui/Sidebar/Sidebar.tsx
+++ src/widgets/MainLayout/ui/Sidebar/Sidebar.tsx
@@ -1,12 +1,20 @@
+import Link from "next/link";
 import cls from "./Sidebar.module.css";
+
+const LINKS: { label: string; href: string }[] = [
+  { label: "Add video", href: "/editor/addVideo" },
+  { label: "Profile", href: "/profile/:videoId" },
+];
 
 export function Sidebar() {
   return (
     <aside className={cls.sidebar}>
       <nav className={cls.nav}>
-        <a href="#">Home</a>
-        <a href="#">Video</a>
-        <a href="#">Profile</a>
+        {LINKS.map(({ label, href }) => (
+          <Link key={href} href={href}>
+            {label}
+          </Link>
+        ))}
       </nav>
     </aside>
   );
