a7b1e020c9ed2d16cbf1a547b968bc4d9d9d0017
pvslvch <polina.vasilevichh@gmail.com>
2026-04-16 14:32:30 +0200

    feat(video-page): redesign UI for a separate video page

--- src/screen/VideoScreen/VideoScreen.tsx
+++ src/screen/VideoScreen/VideoScreen.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import cls from "./VideoScreen.module.css";
 
 type VideoScreenProps = {
@@ -7,17 +8,28 @@ type VideoScreenProps = {
 export const VideoScreen = ({ videoId }: VideoScreenProps) => {
   return (
     <div className={cls.container}>
-      {videoId && (
-        <iframe
-          width="550"
-          height="300"
-          src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-        />
-      )}
+      <iframe
+        width="550"
+        height="300"
+        src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+        className={cls.iframe}
+      />
+
+      <b>VIDEO TITLE</b>
+
+      <div className={cls.videoInfoContainer}>
+        <Link href="/CHANNEL-NAME" className={cls.channelImg}>
+          <div className={cls.hiddenText}>CHANNEL NAME</div>
+        </Link>
+
+        <Link href="/CHANNEL-NAME" className={cls.channelNameLink}>
+          CHANNEL NAME
+        </Link>
+      </div>
     </div>
   );
 };
