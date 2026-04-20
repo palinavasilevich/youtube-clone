27578bc5608e6f91006782fee9d36b29e0e38505
pvslvch <polina.vasilevichh@gmail.com>
2026-04-20 14:53:46 +0200

    feat(metadata): implement dynamic generation of metadata

--- src/app/(public)/[categoryId]/page.tsx
+++ src/app/(public)/[categoryId]/page.tsx
@@ -3,13 +3,27 @@ import { VIDEO_CATEGORIES } from "@/shared/constants/videoCategories";
 import { GetVideosResponse } from "@/shared/types/api.types";
 import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "Video in category: ...",
-};
-
 type CategoryPageProps = {
   params: Promise<{ categoryId: string }>;
 };
+
+export async function generateMetadata({
+  params,
+}: CategoryPageProps): Promise<Metadata> {
+  const { categoryId } = await params;
+
+  const foundCategory = VIDEO_CATEGORIES.find(
+    (category) => category.id === categoryId,
+  );
+
+  if (!foundCategory) {
+    return { title: "Video in unknown category" };
+  }
+
+  return {
+    title: `Video in category: ${foundCategory?.title}`,
+  };
+}
 
 export default async function CategoryPage({ params }: CategoryPageProps) {
   const { categoryId } = await params;
--- src/app/(public)/video/[videoId]/page.tsx
+++ src/app/(public)/video/[videoId]/page.tsx
@@ -2,13 +2,35 @@ import { VideoScreen } from "@/screen/VideoScreen";
 import { GetVideoByIdResponse } from "@/shared/types/api.types";
 import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "Video: ...",
-};
-
 type VideoPageProps = {
   params: Promise<{ videoId: string }>;
 };
+
+export async function generateMetadata({
+  params,
+}: VideoPageProps): Promise<Metadata> {
+  const { videoId } = await params;
+
+  try {
+    const response = await fetch(
+      `${process.env.SERVER_API_URL}/api/video/${videoId}`,
+    );
+    const { data: video } = (await response.json()) as GetVideoByIdResponse;
+
+    if (!video) {
+      throw new Error("No video data available");
+    }
+
+    return {
+      title: `Video: ${video.title}`,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      title: "Something went wrong..",
+    };
+  }
+}
 
 export default async function VideoPage({ params }: VideoPageProps) {
   const { videoId } = await params;
